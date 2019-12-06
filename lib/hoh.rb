05d5eca0196a02fcdc6faa3a1c486dc6b4230c7f
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 20:27:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
 # }
 
 def naming_system
-PORTION_1 = {
+{
   :label => "Kingdom",
    :sub_category => {
      :label => "Phylum",
@@ -41,18 +41,18 @@ PORTION_1 = {
    }
  }
 }
-PORTION_2 = {
+{
     :label => "Order"
  }
  
-PORTION_3 = {
+{
    :label => "Family",
    :sub_category => {
      :label => "Genus",
    }
  }
  
-PORTION_4 = {
+{
    :label => "Species",
    :sub_category => nil
  }
