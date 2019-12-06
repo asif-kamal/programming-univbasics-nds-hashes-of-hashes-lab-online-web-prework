186f2c78cf11f42c11b49b99d46b61ac83f69186
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 20:36:05 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,11 +36,26 @@ def naming_system
    :sub_category => {
      :label => "Phylum",
      :sub_category => {
-       :label => "Class"
+       :label => "Class", 
+       :sub_category => {
+         :label => "Order"
+    }
      }
    }
  }
 
 
+ 
+{
+   :label => "Family",
+   :sub_category => {
+     :label => "Genus",
+   }
+ }
+ 
+{
+   :label => "Species",
+   :sub_category => nil
+ }
  
 end
