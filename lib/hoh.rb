8d8b2b995ca5da2e015317197c1982c10465572a
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 20:42:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,24 +38,21 @@ def naming_system
      :sub_category => {
        :label => "Class", 
        :sub_category => {
-         :label => "Order"
-    }
-     }
-   }
- }
+         :label => "Order",
+          :sub_category => {
+           :label => "Family",
+            :sub_category => {
+             :label => "Genus",
+              :sub_category => {
+                :label => "Species",
+                 :sub_category => {nil
+                 }
+                }
+               }
+              }
+             }
+            }
+           }
+          }
 
-
- 
-{
-   :label => "Family",
-   :sub_category => {
-     :label => "Genus",
-   }
- }
- 
-{
-   :label => "Species",
-   :sub_category => nil
- }
- 
 end
